d8ccfc0d91e0aa5923660ccae42c02a1f1f5b6b1
Md Zeeshan <98448457+mdzeeshan-2@users.noreply.github.com>
2023-06-26 00:02:19 +0530

    Update Banner.jsx

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -1,30 +1,63 @@
-import "./banner.css"
-import { useRef } from "react";
-import SearchInput from "../Search/SearchInput";
-import SearchButton from "../Search/SearchButton";
-const Banner = () =>{
-    const SearchInputRef = useRef();
-    const handleSearch = () =>{
-        alert(`you types ${SearchInputRef.current.value}`)
+import React, { useEffect, useState } from "react";
+
+import axios from "../axios";
+import "./banner.css";
+import requests from "../requests";
+
+function Banner() {
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchTrending);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length)
+        ]
+      );
+      return request;
     }
-    const handleKeyDown = (event) =>{
-        if(event.key==="Enter"){
-         handleSearch();
-        }
-    }
-    return <div className="banner">
-    <div className="heading_banner">
-    <div className="head_head_banner">
-        <h2 className="text_banner">Welcome.</h2>
-        <h3 className="subtext_banner">Millions of movies, TV shows and people to discover. Explore now.</h3>
-    </div> 
-    <div className="parent_container">
-        <SearchInput ref={SearchInputRef} onKeyDown={handleKeyDown}/>
-        <div className="child_container"> 
-        <SearchButton onClick={handleSearch}/>
-        </div>
-    </div>
-    </div>
-    </div>
+    fetchData();
+  }, []);
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `linear-gradient(
+        to right,
+        rgba(3, 37, 65, 0.8) 0%,
+        rgba(3, 37, 65, 0) 100%
+      ), url(
+        "https://image.tmdb.org/t/p/original${movie?.backdrop_path}"
+      )`
+      }}
+    >
+      <div className="banner-contents">
+        <h2 className="banner-title">Welcome.</h2>
+        <h3 className="banner-description">
+          Millions of movies, TV shows and people to discover. Explore now.
+        </h3>
+
+        <form className="inner-search-form" action="/search" method="get">
+          <input
+            className="input-text"
+            dir="auto"
+            name="query"
+            type="text"
+            tabIndex="1"
+            autoCorrect="off"
+            autofill="off"
+            autoComplete="off"
+            spellCheck="false"
+            placeholder="Search for a movie, tv show, person......"
+          />
+
+          <input className="input-submit" type="submit" value="Search" />
+        </form>
+      </div>
+    </header>
+  );
 }
+
 export default Banner;
