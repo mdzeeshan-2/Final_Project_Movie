7ac5d80a37c6fca224b72387f5f2923d2d1c7e40
Md Zeeshan <98448457+mdzeeshan-2@users.noreply.github.com>
2023-12-13 08:51:04 +0530

    Deleted Search section

--- src/Search/SearchButton.jsx
+++
@@ -1,7 +0,0 @@
-import "./SearchButton.css"
-const SearchButton = (props) =>{
-    return <button className="button" {...props}>
-        Search
-    </button>
-}
-export default SearchButton;
--- src/Search/SearchInput.jsx
+++
@@ -1,8 +0,0 @@
-import "./Searchinput.css";
-import { forwardRef } from "react";
-const SearchInput = forwardRef((props, ref) => {
-    return <>
-    <input type="text" {...props} ref={ref} className="input"  placeholder="Search for a movie, tv show, person......" />
-</>
-})
-export default SearchInput;
